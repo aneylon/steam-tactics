8fffd462011cb5ec29e45a77f5c844021e1b5785
aneylon <arlen.m.neylon@gmail.com>
2020-04-13 17:18:17 -0600

    Basic setup.

---
+++ steam-tactics/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public MusicManager musicManager;
+    void Start()
+    {
+        Debug.Log("Start it up.");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ steam-tactics/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Pump up the jams!");
+    }
+
+    void Update()
+    {
+        
+    }
+}

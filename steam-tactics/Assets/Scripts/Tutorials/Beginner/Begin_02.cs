3769a8461ff98238cdc36c56673dc0d3bf0aec8b
aneylon <arlen.m.neylon@gmail.com>
2020-04-14 19:08:42 -0600

    Organize tutorial scripts.

---
+++ steam-tactics/Assets/Scripts/Tutorials/Beginner/Begin_02.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Begin_02 : MonoBehaviour
+{
+    int myNumber = 5;
+
+    void Start()
+    {
+        Debug.Log("Yo! : " + multiplyBy(myNumber, 3));
+    }
+
+    int multiplyBy(int input, int multiplier)
+    {
+        return input * multiplier;
+    }
+
+}
